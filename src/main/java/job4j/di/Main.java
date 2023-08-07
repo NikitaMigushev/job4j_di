48ac646d0a199c47fd02274970e3ee9a82aeaedf
Nikita Migushev <migushev@gmail.com>
2023-08-07 11:45:12 +0300

    0. Что такое DI? [#6876 # [#6876]]

--- src/main/java/job4j/di/Main.java
+++ src/main/java/job4j/di/Main.java
@@ -2,6 +2,16 @@ package job4j.di;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Context context = new Context();
+        context.reg(Store.class);
+        context.reg(ConsoleInput.class);
+        context.reg(StartUI.class);
+
+        StartUI ui = context.get(StartUI.class);
+        ConsoleInput consoleInput = context.get(ConsoleInput.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
     }
 }
